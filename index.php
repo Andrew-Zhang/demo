5647fc9e6875a09446c307ff5318e6af3edc4d5b
AndrewZhang <zkkandrew@gmail.com>
2013-10-23 00:08:26 +0800

    重写了二进制时间和代码，比上一版本简化了一点

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 	a{color:white;}
 	body{
 		background-color:#503151;
-		opacity:0.6;
-		filter:alpha(opacity=60);
+		/*opacity:0.6;
+		filter:alpha(opacity=60);*/
 	}
 	img:hover{
 		-moz-transform:rotate(45deg);
@@ -21,12 +21,12 @@
 	}
 	#welcome font{
 		margin-left:20px;
-		font-size:48px;
+		font-size:42px;
 	}
 	/*CSS of Time*/
 	#time{
 		margin:0 auto;
-		width:636px;
+		width:555px;
 	}
 	#time p{
 		float:left;	
@@ -42,18 +42,11 @@
 	</style>
 </head>
 <body>
-    <p style="text-align:center;font-size:14px;font-family:tahoma,Consolas;margin-top:180px;" id="welcome"><font color="#1943AF">W</font><font color="#B0BDCD">E</font><font color="#0072BC">L</font><font color="#01AAAD">C</font><font color="#EB030D">O</font><font color="#FFC20F">M</font><font color="#7B1B72">E</font><font color="#8CC63E">!</font></p>
-    <p style="text-align:center;font-size:14px;font-family:tahoma,Consolas;margin-top:25px;"><a href="http://weibo.com/u/1736799387" target="_blank">@张亢亢Andrew</a></p>
+    <p style="text-align:center;font-size:14px;font-family:tahoma,Consolas;margin-top:150px;" id="welcome"><font color="#1943AF">Let</font><font color="#B0BDCD">The</font><font color="#0072BC">Space</font><font color="#01AAAD">Ship</font><font color="#EB030D">Take</font><font color="#FFC20F">You</font><font color="#7B1B72">Go</font><font color="#8CC63E">!</font></p>
+    <p style="text-align:center;font-size:14px;font-family:tahoma,Consolas;margin-top:120px;"><a href="http://weibo.com/u/1736799387" target="_blank">@张亢亢Andrew</a></p>
     <p style="text-align:center;font-size:14px;font-family:tahoma,Consolas;margin-top:25px;"><a href="./blog" target="_blank">BLOG</a></p>
-    <p><center><img style="width:50px;" src="./images/SpaceShip.png" title="Let The Space Ship Take You Go!" alt="Space Ship" /></center></p>
-    <div id="time">
-    	<p id="timeYear"></p>
-    	<p id="timeMonth"></p> 
-    	<p id="timeDate"></p> 
-    	<p id="timeHour"></p>
-   		<p id="timeMinute"></p> 
-   		<p id="timeSecond"></p> 
-    </div>
+    <p><center><img style="width:50px;" src="SpaceShip.png" title="Let The Space Ship Take You Go!" alt="Space Ship" /></center></p>
+    <div id="time"> </div>
 </body>
 <!--
 <script type="text/javascript" src="http://js.tongji.linezing.com/3129892/tongji.js"></script><noscript><a><img src="http://img.tongji.linezing.com/3129892/tongji.gif"/></a></noscript>
@@ -63,34 +56,38 @@
 <!--Javascript of Time-->
 <script type="text/javascript">
 $(document).ready(function(){
-function disTime(timeEach,where){//判断二进制有几位则生成几个框，再改变颜色！
-	var arrEach=timeEach.split("");
-	$("#"+where).html("<span></span>");
-	$.each(arrEach,function(i){
-		if(arrEach[i]==='1'){
-			$("#"+where).children().eq(i).css("background-color","white");
-		} else {
-			$("#"+where).children().eq(i).css("background-color","black");	
+	function disTime(timeAll){
+		var _p = '<p></p>';
+		var _span = '<span></span>';
+		var arrTime=timeAll.split("|");
+		$("#time").html(_p);
+		$("#time p").html(_span);
+		for (var j=0;j<6;j++){
+			var arrTimeEach=arrTime[j].split("");
+			$.each(arrTimeEach,function(i){
+				if(arrTimeEach[i]==='1'){
+					$("#time").children().eq(j).children().eq(i).css("background-color","white");
+				} else {
+					$("#time").children().eq(j).children().eq(i).css("background-color","black");	
+				}
+				$("#time").children().eq(j).children().eq(i).after(_span);//判断是否为最后一个，是则显示年！
+			});
+		$("#time").children().eq(j).after(_p);
+		$("#time").children().eq(j+1).html(_span);
 		}
-		$("#"+where).children().eq(i).after("<span></span>");//判断是否为最后一个，是则显示年！
-	});			
-}
-(function timeOK(){
-	var todayTime=new Date();
-	var timeYear=parseInt(todayTime.getFullYear().toString().substr(-2,2)).toString(2);
-	var timeMonth=(todayTime.getMonth()+1).toString(2);	
-	var timeDate=todayTime.getDate().toString(2);
-	var timeHour=todayTime.getHours().toString(2);
-	var timeMinute=todayTime.getMinutes().toString(2);
-	var timeSecond=todayTime.getSeconds().toString(2);
-	disTime(timeYear,'timeYear');
-	disTime(timeMonth,'timeMonth');
-	disTime(timeDate,'timeDate');
-	disTime(timeHour,'timeHour');
-	disTime(timeMinute,'timeMinute');
-	disTime(timeSecond,'timeSecond');
-	setTimeout(timeOK,1000);
-}());
+		
+	}
+	(function timeHandle(){
+		var todayTime=new Date();
+		var timeYear=parseInt(todayTime.getFullYear().toString().substr(-2,2)).toString(2);
+		var timeMonth=(todayTime.getMonth()+1).toString(2);	
+		var timeDate=todayTime.getDate().toString(2);
+		var timeHour=todayTime.getHours().toString(2);
+		var timeMinute=todayTime.getMinutes().toString(2);
+		var timeSecond=todayTime.getSeconds().toString(2);	
+		disTime(timeYear+'|'+timeMonth+'|'+timeDate+'|'+timeHour+'|'+timeMinute+'|'+timeSecond);
+		setTimeout(timeHandle,1000);
+	}());
 });
 </script>
 </html>
